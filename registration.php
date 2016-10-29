dd8d339f25432e37e580ce4131cd876b117e330a
iShinta <ho.minhtri.net@gmail.com>
2016-10-28 20:24:23 -0500

    Back to Basics

--- registration.php
+++ registration.php
@@ -2,30 +2,30 @@
   $name1 = $_POST["name1"];
   //$name2 = $_POST["name2"];
   //$name3 = $_POST["name3"];
-  $name4 = $_POST["name4"];
+  /*$name4 = $_POST["name4"];
   $name5 = $_POST["name5"];
   $name6 = $_POST["name6"];
   $name7 = $_POST["name7"];
   $name8 = $_POST["name8"];
-  $name9 = $_POST["name9"];
+  $name9 = $_POST["name9"];*/
 
   $fh = fopen("signup.txt", "a");
   if($name1 != ""){
     fwrite($fh, 1."\n");
     fwrite($fh, $name1."\n");
   }
-  if($name2 != ""){
+  /*if($name2 != ""){
     fwrite($fh, 2."\n");
     fwrite($fh, $name2."\n");
   }
-  /*if($name3 != ""){
+  if($name3 != ""){
     fwrite($fh, 3."\n");
     fwrite($fh, $name3."\n");
   }
   if($name4 != ""){
     fwrite($fh, 4."\n");
     fwrite($fh, $name4."\n");
-  }*/
+  }
   if($name5 != ""){
     fwrite($fh, 5."\n");
     fwrite($fh, $name5."\n");
@@ -45,7 +45,7 @@
   if($name9 != ""){
     fwrite($fh, 9."\n");
     fwrite($fh, $name9."\n");
-  }
+  }*/
   fclose($fh);
 ?>
 <h1>Thank you for registering</h1>
